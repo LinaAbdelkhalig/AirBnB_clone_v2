f99150ad845acd067fdf7fd60ed8f1c36491dccf
NafisaKaruri <nafeesahassan016@gmail.com>
2024-03-22 15:20:34 -0700

    fixed

--- models/place.py
+++ models/place.py
@@ -8,14 +8,14 @@ from models.amenity import Amenity
 from models.review import Review
 
 place_amenity = Table('place_amenity', Base.metadata,
-                        Column('place_id',
-                               String(60),
-                               ForeignKey('places.id'),
-                               primary_key=True),
-                        Column('amenity_id',
-                               String(60),
-                               ForeignKey('amenities.id'),
-                               primary_key=True))
+                      Column('place_id',
+                             String(60),
+                             ForeignKey('places.id'),
+                             primary_key=True),
+                      Column('amenity_id',
+                             String(60),
+                             ForeignKey('amenities.id'),
+                             primary_key=True))
 
 
 class Place(BaseModel, Base):
