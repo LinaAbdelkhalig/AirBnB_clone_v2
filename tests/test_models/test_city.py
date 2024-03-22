56fb324d80b131353d63b234759bb7b9d7b49fda
Lina Abdelkhalig <lelsammani0@gmail.com>
2024-03-23 01:24:26 +0200

    test city comment

--- tests/test_models/test_city.py
+++ tests/test_models/test_city.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-""" """
+"""the test module for the city class"""
 from tests.test_models.test_base_model import test_basemodel
 from models.city import City
 
 
 class test_City(test_basemodel):
-    """ """
+    """ the test for the city class"""
 
     def __init__(self, *args, **kwargs):
         """ """
