5e723d41097b964f87b4670c406979c623466ffd
Lina Abdelkhalig <lelsammani0@gmail.com>
2024-04-24 00:59:53 +0200

    fix

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""
+""" 
 this script start a flask web app that listens on port 0.0.0.0
 port 5000
 """
@@ -7,8 +7,6 @@ port 5000
 from flask import Flask
 
 app = Flask(__name__)
-
-
 @app.route('/', strict_slashes=False)
 def hello():
     """
@@ -17,6 +15,7 @@ def hello():
     """
     return "Hello HBNB!"
 
+@app.route('/hbnb', strict_slashes=False)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
